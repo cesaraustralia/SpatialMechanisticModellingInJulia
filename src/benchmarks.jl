59e67437547d13e0b172629b160c62f01d7b385b
Rafael Schouten <rafaelschouten@gmail.com>
2020-11-29 15:32:59 +1100

    working benchmarking on all hardware

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -1,133 +1,146 @@
-using BenchmarkTools
-
-#### Benchmarks ##########################################################
-
-# Wind dispersal model For GPU randomisation
-
-# As we don't have `rand()` inside the kernel, we have to calulate it
-# in another grid from outside a kernel, using a `Grid` rul.e
-# We want rand to be padded with a neighborhood so we can sample
-# extra values.
-
-using Random
-randomgrid = Grid{Tuple{},:rand}() do w
-    if w isa CuArray
-        CUDA.rand!(w)
-    else
-        Random.rand!(w)
-    end
-end
-# init = zeros(Float32, 1000, 1000)
-# o = ResultOutput((; rand=zero(hostpop)); tspan=1:100)
-# @time sim!(o, randomgrid; proc=CuGPU())
-
-# @btime sim!($o, $randomgrid; proc=CuGPU())
-# @time sim!(o, randomgrid)
-# @btime sim!($o, $randomgrid)
-
-gpu_wind = SetNeighbors{Tuple{:rand,:H},Tuple{:rand,:H}}() do data, hood, index, rand, state
-    # Ignore empty cells
-    state > zero(state) || return nothing
-    # Randomise a dispersal event using cell value of rand,
-    # calculated in the rand Grid rule
-    # rand < 0.2 || return nothing
-    # Randomise a destination
-    # spotrange = 10
-    # Get random values for jump from the surrounding grid
-    # randi = (index[1], index[2] + 1)
-    # randj = (index[1] + 1, index[2] + 1)
-    # jump = round.(Int, (data[:rand][randi...], data[:rand][randj...]) .* 2spotrange .- spotrange)
-    # Make sure the destination is on the grid
-    # jumpdest, is_inbounds = inbounds(jump .+ index, data[:host])
-    # Update spotted cell if it's on the grid
-    # is_inbounds && @inbounds add!(data[:host], state, jumpdest...)
-    return nothing
-end
-
-# output = ArrayOutput((; host=hostpop, rand=zero(hostpop)); tspan=tspan)
-# sim!(output, (randomgrid, gpu_wind))
-# @btime sim!($output, $wind);
-# @btime sim!($output, $wind; proc=ThreadedCPU());
-# @btime sim!($output, $randomgrid, $gpu_wind; proc=CuGPU());
-
-procs = Dict("single"=>SingleCPU(), "threaded"=>ThreadedCPU())
-opts = Dict("noopt"=>NoOpt(), "sparseopt"=>SparseOpt())
-sizes = (100, 200, 400)#, 1000, 2000)
+using BenchmarkTools, Plots, KernelAbstractions, CUDA, Random
 
 function setupsim(rules;
     tspan=DateTime(2020, 1, 1):Day(1):DateTime(2020, 4, 10),
     size=(100, 100), opt=NoOpt(), proc=SingleCPU(),
-    growthmin=-5.0, growthmax=0.2
+    growthmin=-5.0f0, growthmax=0.2f0
 )
-    growthrates = rand(size...) .* (growthmax - growthmin) .- growthmin
-    init = (; H=zeros(size...))
-    o = ResultOutput(init;
-        aux=(; rH=growthrates),
-        mask=parent(mask),
-        tspan=tspan,
+    # gr = rand(Float32, size...) .* (growthmax - growthmin) .- growthmin
+    # init = (; H=rand(Bool, size) .* 100.0f0, rand=rand(Float32, size))
+    hostpop .= 100.0f0
+    rnge = Base.OneTo.(size) 
+    o = ArrayOutput((; H=parent(hostpop)[rnge...], P=parent(parapop)[rnge...], rand=rand(Float32, size)); 
+        aux=(; rH=r_host_aus[rnge..., :], rP=r_para_aus[rnge..., :]), 
+        mask=Array(mask)[rnge...],
+        tspan=tspan
     )
     rs = Ruleset(rules; opt=opt, proc=proc)
-    sd = DynamicGrids.SimData(DynamicGrids.extent(o), rs)
+    # sd = DynamicGrids.SimData(DynamicGrids.extent(o), rs)
     # Warmup compilation
-    sim!(o, rs; simdata=sd)
-    return (; o, rs, sd)
+    # sim!(o, rs)
+    return (; o, rs)
 end
 function bench(rules)
-    # procs = Dict("gpu"=>CuGPU(), "single"=>SingleCPU(), "threaded"=>ThreadedCPU())
-    procs = Dict("single"=>SingleCPU(), "threaded"=>ThreadedCPU())
-    opts = Dict("noopt"=>NoOpt(), "sparseopt"=>SparseOpt())
-    sizes = (100, 200, 300, 400)#, 1000, 2000)
     suite = BenchmarkGroup()
-    suite[:noopt] = BenchmarkGroup()
-    suite[:sparseopt] = BenchmarkGroup()
     for opt in keys(opts)
         suite[opt] = BenchmarkGroup(["proc", "size"])
         for proc in keys(procs), s in sizes
-            suite[opt][proc, s] = @benchmarkable sim!(x[:o], x[:rs]; simdata=x[:sd]) setup=(x = setupsim($rules; size=($s, $s), proc=$(procs[proc]), opt=$(opts[opt])))
+            suite[opt][proc, s] = @benchmarkable sim!(x[:o], x[:rs]) setup=(x = setupsim($rules; size=($s, $s), proc=$(procs[proc]), opt=$(opts[opt])))
         end
     end
     results = run(suite, verbose=true)
 end
-function plotbench(b)
-    p1 = plot(; title="SparseOpt")
-    map(Tuple(keys(procs))) do prockey
-        times = map(sizes) do s
-            minimum(b.data["sparseopt"].data[(prockey, s)].times)
-        end
-        plot(p1, collect(sizes), collect(times) .* 10e-6; label="sparse $prockey")
-    end
-    p2 = plot(; title="NoOpt")
-    map(Tuple(keys(procs))) do prockey
-        times = map(sizes) do s
-            minimum(b.data["noopt"].data[(prockey, s)].times)
-        end
-        plot!(p2, collect(sizes), collect(times) .* 10e-6; serieslabel=prockey)
-    end
-    plot(p1, p2; yaxis=:log);
+
+procs = Dict("gpu"=>CuGPU(), "single"=>SingleCPU(), "threaded"=>ThreadedCPU())
+opts = Dict("noopt"=>NoOpt(), "sparseopt"=>SparseOpt())
+sizes = (100, 200, 300, 400)#, 1000, 2000)
+rulegroups = (Wind=(randomgrid, gpu_wind, growth), Local=(localdisp, growth), Combined=(localdisp, randomgrid, gpu_wind, growth))
+results = map(bench, rulegroups)
+results[:Combined][@tagged 400]
+minimum(results[:Combined].data["sparseopt"].data[("gpu", 400)].times) / 1e9
+
+output = ArrayOutput((; H=parent(hostpop), P=parent(parapop), rand=rand(Float32, size(hostpop))); 
+    aux=(; rH=r_host_aus, rP=r_para_aus), 
+    mask=Array(mask),
+    tspan=tspan
+)
+for key in keys(rulegroups), p in values(procs), o in values(opts)
+    @show key p o
+    @time sim!(output, rulegroups[key]; proc=p)
 end
 
-rulegroups = (wind=(wind, growth), loc=(localdisp, growth), combined=(localdisp, wind, growth))
-results = map(bench, rulegroups)
-results[:wind][@tagged 400]
-plotbench(results[:wind])
+function plotbench(b, key)
+    p = plot(; )
+    for prockey in Tuple(keys(procs))
+        if prockey != "gpu" # no sparseopt on GPU
+            so_times = map(sizes) do s
+                minimum(b.data["sparseopt"].data[(prockey, s)].times)  / 1e9
+            end
+            @show so_times
+            plot!(p, collect(sizes), collect(so_times); label="Sparse $prockey", pallete=:Dark2_5, opacity=0.8)
+        end
+        no_times = map(sizes) do s
+            minimum(b.data["noopt"].data[(prockey, s)].times) / 1e9
+        end
+        @show no_times
+        plot!(p, collect(sizes), collect(no_times); label=uppercasefirst(prockey), pallete=:Dark2_5, opacity=0.8)
+    end
+    plot(p; 
+        title=string(key), 
+        ylims=(0, 0.4), 
+        legend=:top, 
+        tickfontcolor=RGB(0.2),
+        legendfontcolor=RGB(0.2),
+        minorgrid=false,
+        xlabel="Grid side",
+        ylabel="Time in seconds",
+    )
+end
+pyplot()
+theme(:vibrant)
+using Plots: px
+plot(map(plotbench, results, keys(results))...; layout=(1, 3), size=(1200, 400))
 
+savefig("benchmarks.png")
 
 
 ### Other ########################################33
 
+using ConstructionBase
+ConstructionBase.constructorof(::Type{<:LinRange}) = _linrange
+_linrange(start, stop, len, lendiv) = LinRange(start, stop, len)
 
-# output = ArrayOutput((; host=hostpop); aux=(; rH=growthrates1), tspan=tspan)
-# sim!(output, (localdisp, growth, viewer))
-# sim!(output, (wind, growth, viewer))
+hostpop_disag, parapop_disag, r_host_aus_disag, r_para_aus_disag, mask_disag = 
+    map((hostpop, parapop, r_host_aus, r_para_aus, mask)) do A
+        A = set(A, Lat=NoIndex(), Lon=NoIndex())
+        # A = hasdim(A, Ti) ? set(A, Ti=NoIndex()) : A
+        GeoData.disaggregate(Center(), A, (Lat(4), Lon(4), Ti(1)))
+    end
 
-# @time sim!(output, growth);
-# @time sim!(output, localdisp, growth);
-# @time sim!(output, wind, localdisp, growth);
-# @time sim!(output, wind, growth);
-# @time sim!(output, localdisp, growth; proc=ThreadedCPU());
-# @time sim!(output, wind, growth; proc=ThreadedCPU());
-# @time sim!(output, localdisp, growth; proc=CuGPU());
-# @time sim!(output, wind, growth; proc=CuGPU());
+A = set(r_host_aus, Lat=NoIndex(), Lon=NoIndex(), Ti=NoIndex())
+typeof(index(r_host_aus_disag))
+
+output = ArrayOutput((; H=parent(hostpop_disag), P=parent(parapop_disag), rand=rand(Float32, size(hostpop_disag))); 
+    aux=(; rH=r_host_aus_disag, rP=r_para_aus_disag), mask=Array(mask_disag), tspan=tspan)
+
+@time sim!(output, growth);
+@time sim!(output, growth; proc=ThreadedCPU());
+@time sim!(output, growth; proc=CuGPU());
+@time sim!(output, localdisp);
+@time sim!(output, localdisp; proc=ThreadedCPU());
+@time sim!(output, localdisp; proc=CuGPU());
+@time sim!(output, localdisp, growth);
+@time sim!(output, localdisp, growth; proc=ThreadedCPU());
+@time sim!(output, localdisp, growth; proc=CuGPU());
+@time sim!(output, gpu_wind, randomgrid, growth);
+@time sim!(output, gpu_wind, randomgrid, growth; proc=ThreadedCPU());
+@time sim!(output, gpu_wind, randomgrid, growth; proc=CuGPU());
+@time sim!(output, localdisp, gpu_wind, randomgrid, growth);
+@time sim!(output, localdisp, gpu_wind, randomgrid, growth; proc=ThreadedCPU());
+@time sim!(output, localdisp, gpu_wind, randomgrid, growth; proc=CuGPU());
+
+@time sim!(output, growth, opt=SparseOpt());
+@time sim!(output, growth; proc=ThreadedCPU(), opt=SparseOpt());
+@time sim!(output, growth; proc=CuGPU(), opt=SparseOpt());
+@time sim!(output, localdisp, growth, opt=SparseOpt());
+@time sim!(output, localdisp, growth; proc=ThreadedCPU(), opt=SparseOpt());
+@time sim!(output, localdisp, growth; proc=CuGPU(), opt=SparseOpt());
+@time sim!(output, gpu_wind, randomgrid, growth, opt=SparseOpt());
+@time sim!(output, gpu_wind, randomgrid, growth; proc=ThreadedCPU(), opt=SparseOpt());
+@time sim!(output, gpu_wind, randomgrid, growth; proc=CuGPU(), opt=SparseOpt());
+@time sim!(output, localdisp, gpu_wind, randomgrid, growth, opt=SparseOpt());
+@time sim!(output, localdisp, gpu_wind, randomgrid, growth; proc=ThreadedCPU(), opt=SparseOpt());
+@time sim!(output, localdisp, gpu_wind, randomgrid, growth; proc=CuGPU(), opt=SparseOpt());
+
+ruleset = (randomgrid, gpu_wind, localdisp, para_localdisp, host_para_growth)
+@time sim!(output, ruleset);
+@time sim!(output, ruleset; proc=ThreadedCPU());
+@time sim!(output, ruleset; proc=CuGPU());
+# using ProfileView, Profile
+# Profile.clear()
+# f(output, growth) = @profile for i in 1:100 sim!(output, randomgrid, gpu_wind, growth) end
+# f(output, growth)
+# ProfileView.view()
 
 # output = ArrayOutput((; host=hostpop); tspan=tspan)
 # error()
@@ -140,7 +153,6 @@ plotbench(results[:wind])
 # @btime sim!($output, Chain($localdisp, $growth));
 # @btime sim!($output, Chain($localdisp, $growth); proc=ThreadedCPU());
 # @btime sim!($output, Chain($localdisp, $growth); proc=CuGPU());
-
 # # * use a `let` block for 0.2 so the variable is locally-scope
 
 # output = ArrayOutput((; host=hostpop); aux=(; growthrates=growthrates1), tspan=tspan)
--- src/models.jl
+++ src/models.jl
@@ -1,8 +1,6 @@
 #### Simple Growth Model #####################
 
-using GrowthMaps, CUDA, Plots, Unitful, GeoData, HDF5, Dates, DynamicGridsInteract, DynamicGrids
-using DynamicGrids: auxval
-const DG = DynamicGrids
+using GrowthMaps, Unitful, GeoData, HDF5, Dates, CUDA, Random, KernelAbstractions
 using GeoData: Between
 CUDA.allowscalar(false)
 
@@ -36,6 +34,7 @@ temps = collect(-15.0u"°C":0.1u"K":40.0u"°C")
 days = 1 # one day a month
 series = SMAPseries("/home/raf/Data/SMAP/SMAP_L4_SM_gph_v4")[Where(t -> dayofmonth(t) in days)]
 
+
 # Run for 12 months of 2017, on a CUDA GPU
 @time rates = mapgrowth(host_model, para_model;
     series=series,
@@ -47,35 +46,35 @@ r_host_aus, r_para_aus = map(rates) do gr
     gr[aus...] |> gr -> replace_missing(gr, 0.0f0) |> gr -> permutedims(gr, (Lat, Lon, Ti))
 end
 
-struct IntrinsicGrowth{Ic,U,If,D} <: GrowthModel
-    intercept::Ic
-    up::U
-    inflection::If
-    down::D
-end
+# struct IntrinsicGrowth{Ic,U,If,D} <: GrowthModel
+#     intercept::Ic
+#     up::U
+#     inflection::If
+#     down::D
+# end
 
-GrowthMaps.rate(m::IntrinsicGrowth, temp) = 
-    gr(m.intercept, m.up, m.inflection, m.down, temp)
-gr(a, b, c, d, x) = x < c ? a + b*x  : a + b*c - d*(x - c)  
-host_growth = IntrinsicGrowth(-14.6, 0.05u"K^-1", 30u"°C" |> u"K", 0.1u"K^-1")
-para_growth = IntrinsicGrowth(-14.6, 0.05u"K^-1", 26u"°C" |> u"K", 0.1u"K^-1")
+# GrowthMaps.rate(m::IntrinsicGrowth, temp) = 
+#     gr(m.intercept, m.up, m.inflection, m.down, temp)
+# gr(a, b, c, d, x) = x < c ? a + b*x  : a + b*c - d*(x - c)  
+# host_growth = IntrinsicGrowth(-14.6, 0.05u"K^-1", 30u"°C" |> u"K", 0.1u"K^-1")
+# para_growth = IntrinsicGrowth(-14.6, 0.05u"K^-1", 26u"°C" |> u"K", 0.1u"K^-1")
 
-host_model = Model(Layer(:surface_temp, u"K", host_growth))
-para_model = Model(Layer(:surface_temp, u"K", para_growth))
+# host_model = Model(Layer(:surface_temp, u"K", host_growth))
+# para_model = Model(Layer(:surface_temp, u"K", para_growth))
 
-series = SMAPseries("/home/raf/Data/SMAP/SMAP_L4_SM_gph_v4")
+# series = SMAPseries("/home/raf/Data/SMAP/SMAP_L4_SM_gph_v4")
 
-# Run for 12 months of 2017, on a CUDA GPU
-@time rates = mapgrowth(host_model, para_model;
-    series=series,
-    tspan=DateTime(2017, 1):Month(1):DateTime(2017, 12),
-    arraytype=CuArray, # Run on a CUDA GPU
-)
+# # Run for 12 months of 2017, on a CUDA GPU
+# @time rates = mapgrowth(host_model, para_model;
+#     series=series,
+#     tspan=DateTime(2017, 1):Month(1):DateTime(2017, 12),
+#     arraytype=CuArray, # Run on a CUDA GPU
+# )
 
-aus = Lon(Between(113.3402, 153.9523)), Lat(Between(-43.62234, -10.65125))
-r_host_aus, r_para_aus = map(rates) do gr
-    gr[aus...] |> gr -> replace_missing(gr, 0.0f0) |> gr -> permutedims(gr, (Lat, Lon, Ti))
-end
+# aus = Lon(Between(113.3402, 153.9523)), Lat(Between(-43.62234, -10.65125))
+# r_host_aus, r_para_aus = map(rates) do gr
+#     gr[aus...] |> gr -> replace_missing(gr, 0.0f0) |> gr -> permutedims(gr, (Lat, Lon, Ti))
+# end
 
 # ------------------
 # Implications, useful for all of SDM modelling as a side effect
@@ -83,11 +82,13 @@ end
 # Growth model for the dispersal simulation,
 # using the growth rates we have just generated:
 # Here we use a pre-defined growth model LogisticGrowthMap from Dispersal.jl
-using Dispersal, ColorSchemes, BenchmarkTools
+using Dispersal, ColorSchemes, BenchmarkTools, DynamicGridsInteract, DynamicGrids
+const DG = DynamicGrids
+using DynamicGrids: auxval
 import Dispersal: applyrule, aux
 carrycap = 1f9
-growth = LogisticGrowthMap{:H,:H}(
-    auxkey=Val(:rH),
+growth = LogisticGrowth{:H,:H}(
+    rate=Aux(:rH),
     carrycap=carrycap,
     timestep=Day(1),
 )
@@ -96,7 +97,7 @@ growth = LogisticGrowthMap{:H,:H}(
 λ = Param(0.125f0; bounds=(0.01, 0.3))
 radius = 1
 hood = DispersalKernel{radius}(; formulation=ExponentialKernel(λ), cellsize=1.0f0)
-localdisp = InwardsPopulationDispersal{:H,:H}(hood)
+localdisp = InwardsDispersal{:H,:H}(hood)
 
 # Output 
 hostpop = zero(r_host_aus[Ti(1)])
@@ -133,30 +134,65 @@ wind = Manual{:H,:H}() do data, I, state
     jumpdest, is_inbounds = inbounds(jump .+ I, gridsize(data), RemoveOverflow())
     # Update spotted cell if it's on the grid
     if is_inbounds 
-        @inbounds add!(data[:H], state / 5, jumpdest...)
-        @inbounds sub!(data[:H], state / 5, I...)
+        @inbounds add!(data[:H], state / 10, jumpdest...)
+        @inbounds sub!(data[:H], state / 10, I...)
     end
     return nothing
 end
 
-using DynamicGridsGtk
-viewer = AuxCopy{:rH}(Val(:rH))
-output = GtkOutput((; H=hostpop, rH=zero(r_host_aus[Ti(1)])); 
-    filename="sim.gif", 
-    aux=(; rH=r_host_aus),
-    mask=parent(mask),
-    tspan=tspan, 
-    fps=50,
-    minval=(0.0f0, 0.0f0), 
-    maxval=(carrycap, maximum(r_host_aus)),
-    scheme=(ColorSchemes.autumn, ColorSchemes.inferno),
-)
-sim!(output, (localdisp, growth, viewer))
-DynamicGrids.savegif("localdisp.gif", output)
-sim!(output, (wind, growth, viewer))
-DynamicGrids.savegif("winddisp.gif", output)
-sim!(output, (wind, localdisp, growth, viewer))
-DynamicGrids.savegif("combinded.gif", output)
+# GPU wind
+randomgrid = Grid{Tuple{},:rand}() do w
+    _rand(parent(w))
+end
+
+_rand(w::CuArray) = CUDA.rand!(w)
+_rand(w) = rand!(w)
+
+gpu_wind = SetNeighbors{Tuple{:rand,:H},Tuple{:rand,:H}}() do data, hood, I, rand, H
+    # Ignore empty cells
+    H > zero(H) || return nothing
+    # Randomise a dispersal event using cell value of rand,
+    # calculated in the rand Grid rule
+    rand < 0.02f0 || return nothing
+    # Randomise a destination
+    x = 10oneunit(H)
+    # Get random values for jump from the surrounding grid
+    randi = (I[1], I[2] + 1)
+    randj = (I[1] + 1, I[2] + 1)
+    jump = unsafe_trunc(Int, data[:rand][randi...] * 2x - x), 
+           unsafe_trunc(Int, data[:rand][randj...] * 2x - x)
+    # Make sure the destination is on the grid
+    jumpdest, is_inbounds = inbounds(jump .+ I, data[:H])
+    # Update spotted cell if it's on the grid
+    if is_inbounds 
+        @inbounds add!(data[:H], H / 10, jumpdest...)
+        @inbounds sub!(data[:H], H / 10, I...)
+    end
+    return nothing
+end
+
+
+# viewer = AuxCopy{:rH}(Aux(:rH))
+# output = ElectronOutput((H=hostpop, P=parapop, rand=zero(hostpop)); 
+#     ruleset = Ruleset(wind, localdisp, growth, viewer),
+#     filename="sim.gif", 
+#     aux=(; rH=r_host_aus),
+#     mask=parent(mask),
+#     tspan=tspan, 
+#     store=true,
+#     fps=50,
+#     minval=(0.0f0, 0.0f0, 0.0f0), 
+#     maxval=(carrycap, maximum(r_host_aus), 1.0f0),
+#     scheme=(ColorSchemes.autumn, ColorSchemes.inferno, ColorSchemes.inferno),
+# )
+
+# sim!(output, (localdisp, growth, viewer));
+
+# DynamicGrids.savegif("localdisp.gif", output)
+# sim!(output, (wind, growth, viewer))
+# DynamicGrids.savegif("winddisp.gif", output)
+# sim!(output, (wind, localdisp, growth, viewer))
+# DynamicGrids.savegif("combinded.gif", output)
 
 
 #### Host/Parasite growth model #####################
@@ -193,23 +229,23 @@ Hsat = Param(1e7; bounds=(0.0, 1e10))
 Hhalf = Param(1e6; bounds=(0.0, 1e10))
 host_para_growth = HostParasiteGrowth{Tuple{:H,:P},Tuple{:H,:P}}(γ, ρ, Hsat, Hhalf)
 
-λ = 0.05
+λ = 0.15
 radius = 1
 para_hood = DispersalKernel{radius}(; formulation=ExponentialKernel(λ))
-para_localdisp = InwardsPopulationDispersal{:P,:P}(para_hood)
+para_localdisp = InwardsDispersal{:P,:P}(para_hood)
 
-parapop = hostpop / 3 #(a -> (r = rand(Float32); r < 0.01 ? carrycap : 0.0f0)).(hostpop)
-ruleset = Ruleset(wind, para_localdisp, host_para_growth)
-output = GtkOutput((H=hostpop, P=parapop); 
+parapop = 10ones(Float32, size(hostpop))#(a -> (r = rand(Float32); r < 0.01 ? carrycap : 0.0f0)).(hostpop)
+ruleset = Ruleset(gpu_wind, randomgrid, localdisp, para_localdisp, host_para_growth; proc=CuGPU())
+output = ElectronOutput((H=hostpop, P=parapop, rand=zero(hostpop)); 
     filename="sim.gif", 
     ruleset=ruleset,
     aux=(; rH=r_host_aus, rP=r_para_aus),
     mask=parent(mask),
     tspan=tspan, 
     fps=10,
-    minval=(0.0f0, 0.0f0), 
-    maxval=(carrycap, carrycap),
-    scheme=(ColorSchemes.autumn, ColorSchemes.autumn),
+    minval=(0.0f0, 0.0f0, 0.0f0), 
+    maxval=(carrycap, carrycap, 1.0f0),
+    scheme=(ColorSchemes.autumn, ColorSchemes.autumn, ColorSchemes.autumn),
 )
 display(output)
 sim!(output, ruleset)
